c921c9f084b01e7b5cbd767dc0d0d0719db7dbfe
QIXIUQX <qingwu13950520@outlook.com>
2023-06-10 18:30:22 +0800

    initial commit

---
+++ WeiBoHotSearchAdBlock.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         新浪微博热搜榜关键词屏蔽
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  屏蔽微博热搜榜中tag为：剧集、综艺等明显买量条目,热搜广告,热搜关键词，可自定义标签及关键词
+// @author       QIXIUQX
+// @match        https://weibo.com/*
+// @icon         https://www.sina.com.cn/favicon.ico
+// @grant        none
+// @license      GPL-3.0 License
+// @require      https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+(function () {
+
+})();
